8eaeee33ccca18b663af6f23d01489088f86b18a
Bill <bill.chandos@gmail.com>
2020-10-04 19:57:13 -0700

    Add wind compass

--- src/components/DailyForecast.vue
+++ src/components/DailyForecast.vue
@@ -2,20 +2,20 @@
   <div class="flex justify-center">
     <div class="bg-teal-200 md:w-5/12 p-4 h-full w-11/12">
       <span class="text-xl font-semibold">
-        Hourly Forecast
+        Daily Forecast
       </span>
       <div v-if="!forecasts.length" class="flex justify-between items-center bg-teal-100 px-2 py-4 m-2 rounded-md shadow-md">
         <span>Loading ... </span>
       </div>
-      <div v-for="hour in forecasts" :key="hour.number" class="flex justify-between items-center bg-teal-100 px-2 py-4 m-2 rounded-md shadow-md">
-        <div class="flex-1 text-md font-semibold">{{ prettyDate(hour.startTime) }}</div>
-        <div class="flex-1 text-sm">{{ hour.temperature }}°{{ hour.temperatureUnit }}</div>
-        <div class="flex-1 text-md">{{ hour.shortForecast }}</div>
+      <div v-for="day in forecasts" :key="day.number" class="flex justify-between items-center bg-teal-100 px-2 py-4 m-2 rounded-md shadow-md">
+        <div class="flex-1 text-md font-semibold">{{ day.name }}</div>
+        <div class="flex-1 text-sm">{{ day.temperature }}°{{ day.temperatureUnit }}</div>
+        <div class="flex-1 text-md">{{ day.shortForecast }}</div>
         <div class="flex-1 text-md">
-          {{ hour.windSpeed }}
-          <WindCompass :direction="hour.windDirection" />
+          {{ day.windSpeed }}
+          <WindCompass :direction="day.windDirection" />
         </div>
-        <img :src="hour.icon" :alt="hour.shortForecast" class="rounded-full shadow-md h-8 w-8" style="filter: saturate(40%);">
+        <img :src="day.icon" :alt="day.shortForecast" class="rounded-full shadow-md h-8 w-8" style="filter: saturate(40%);">
       </div>
     </div>
   </div>
@@ -37,24 +37,15 @@ export default {
     const forecasts = ref([]);
     
     const getForecasts = async () => {
-      const response = await fetch('https://api.weather.gov/gridpoints/PQR/87,38/forecast/hourly');
+      const response = await fetch('https://api.weather.gov/gridpoints/PQR/87,38/forecast');
       const json = await response.json()
       forecasts.value = json['properties']['periods'];
-    }
-
-    const prettyDate = (date) => {
-      return new Date(date).toLocaleString('en-US', {month: 'short', day: 'numeric', hour: 'numeric'});
     }
     
     onMounted(getForecasts);
 
-    const icons = {
-      
-    }
-
     return {
       forecasts,
-      prettyDate
     }
   }
 
---
+++ src/components/WindCompass.vue
@@ -0,0 +1,39 @@
+<template>
+  <img src="../assets/icons/arrow.svg" class="inline w-4 h-4" :style="{ transform: rotation }" />
+  
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+    props: {
+        direction: String,
+    },
+    setup(props) {
+        const rotation = ref('');
+        const directionalLookup = {
+            'N': 0,
+            'NNE': 22.5,
+            'NE': 45,
+            'ENE': 67.5,
+            'E': 90,
+            'ESE': 112.5,
+            'SE': 135,
+            'SSE': 157.5,
+            'S': 180,
+            'SSW': 202.5,
+            'SW': 225,
+            'WSW': 247.5,
+            'W': 270,
+            'WNW': 292.5,
+            'NW': 315,
+            'NNW': 337.5
+        }
+        rotation.value = `rotate(${directionalLookup[props.direction]-45}deg)`;
+        return {
+            rotation,
+        }
+    }
+}
+</script>
