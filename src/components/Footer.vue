08c745c8018bd713393cc1a1f56d6ae7289a0393
bchandos <contact@billchandos.dev>
2021-10-24 17:45:35 +0000

    update copyright

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="font-semibold fixed bottom-0 w-full text-right border-t border-teal bg-gray-400 py-2 px-4">
-    &#169; 2020 <a class="underline text-blue-900" href="https://billchandos.dev">billchandos.dev</a>
+    &#169; 2020-21 <a class="underline text-blue-900" href="https://billchandos.dev">billchandos.dev</a>
   </footer>
 </template>
 
