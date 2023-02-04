46ca7a68bd97f7a0750f31a66849419bca87b287
bchandos <contact@billchandos.dev>
2023-02-03 18:44:43 -0800

    dynamic footer copyright year

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,21 @@
 <template>
-  <footer class="font-semibold fixed bottom-0 w-full text-right border-t border-teal bg-gray-400 py-2 px-4">
-    &#169; 2020-21 <a class="underline text-blue-900" href="https://billchandos.dev">billchandos.dev</a>
+  <footer
+    class="font-semibold fixed bottom-0 w-full text-right border-t border-teal bg-gray-400 py-2 px-4"
+  >
+    &#169; 2020-{{ new Date().getFullYear() }}
+    <a class="underline text-blue-900" href="https://billchandos.dev"
+      >billchandos.dev</a
+    >
   </footer>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref } from "vue";
 
 export default {
-  props: {
-  },
+  props: {},
   setup(props) {
-    return {
-    }
-  }
-}
+    return {};
+  },
+};
 </script>
