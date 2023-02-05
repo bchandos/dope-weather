5a1b174c0b7683158f3a4c41092b7c36832e8cf9
bchandos <contact@billchandos.dev>
2023-02-05 09:00:30 -0800

    attempt to fix router in prod

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Daily from '../components/DailyForecast.vue';
-import Hourly from '../components/HourlyForecast.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Daily from "../components/DailyForecast.vue";
+import Hourly from "../components/HourlyForecast.vue";
 
-const routerHistory = createWebHistory()
+const routerHistory = createWebHistory("/dope-weather");
 
 const router = createRouter({
-    history: routerHistory,
-    routes: [
-      {
-        path: '/',
-        component: Daily
-      },
-      {
-        path: '/hourly',
-        component: Hourly
-      },
-      {
-        path: '/daily',
-        component: Daily
-      }
-    ]
-  });
+  history: routerHistory,
+  routes: [
+    {
+      path: "/",
+      component: Daily,
+    },
+    {
+      path: "/hourly",
+      component: Hourly,
+    },
+    {
+      path: "/daily",
+      component: Daily,
+    },
+  ],
+});
 
 export default router;
